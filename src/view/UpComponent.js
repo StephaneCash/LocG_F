899e79e046bfe0e421ff52fce44d9b2c359abde0
StephaneCash <kikonistephane@gmail.com>
2021-12-12 23:31:56 +0100

    Déconnexion

--- src/view/UpComponent.js
+++ src/view/UpComponent.js
@@ -1,6 +1,8 @@
 import cash from "../img/cash.jpg";
 import { useState } from "react";
-import { Link } from "react-router-dom"
+import { Link, useHistory } from "react-router-dom";
+import swal from 'sweetalert';
+import axios from 'axios';
 
 function UpComponent(props) {
 
@@ -9,6 +11,21 @@ function UpComponent(props) {
     var data = props.valueInput;
 
     data = valueInput;
+
+    let history = useHistory();
+
+    const logoutSubmit = (e) => {
+        e.preventDefault();
+
+        axios.post(`/api/logout`).then(res => {
+            if (res.data.status === 200) {
+                localStorage.removeItem('auth_token');
+                localStorage.removeItem('auth_name');
+                swal("Success", res.data.message, "success");
+                history.push('/');
+            }
+        });
+    }
 
     return (
         <>
@@ -24,7 +41,7 @@ function UpComponent(props) {
                         <span className="fa fa-comment"></span>
                         <div>
                             <img src={cash} />
-                            <button type="button" className="btn btn-default ml-2" style={{border: "1px solid silver"}}>
+                            <button type="button" onClick={logoutSubmit} className="btn btn-default ml-2" style={{ border: "1px solid silver" }}>
                                 Déconnexion <i className="fa fa-power-off" style={{ color: "#282c34", fontSize: "19px" }}></i>
                             </button>
                         </div>
